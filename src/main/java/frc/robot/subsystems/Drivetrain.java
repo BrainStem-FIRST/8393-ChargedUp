d26b3a68b6377ec6d3e06fe2ef76598756a547fb
Mihir Sharma <mihirsharma700@gmail.com>
2023-01-07 15:53:31 -0500

    added subsystems

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drivetrain extends SubsystemBase {
+  public Drivetrain() {}
+
+  public CommandBase exampleMethodCommand() {
+    return runOnce(
+        () -> {
+
+        });
+  }
+
+  public boolean exampleCondition() {
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+
+  }
+
+  @Override
+  public void simulationPeriodic() {
+
+  }
+}
+
