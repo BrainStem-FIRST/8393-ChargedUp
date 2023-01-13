80ccb0306d707b009504dec1eb035437654b52c8
Mihir Sharma <mihirsharma700@gmail.com>
2023-01-13 17:25:05 -0500

    swerve module extends subsystem base

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -81,8 +81,7 @@ public class Drivetrain extends SubsystemBase {
         public static final double driveFeedForward = 0.0;
 
         /* Drive Motor Characterization Values */
-        public static final double driveKS = (0.667 / 12); // divide by 12 to convert from volts to percent output for
-                                                           // CTRE
+        public static final double driveKS = (0.667 / 12); // divide by 12 to convert from volts to percent output for                                   // CTRE
         public static final double driveKV = (2.44 / 12);
         public static final double driveKA = (0.27 / 12);
 
