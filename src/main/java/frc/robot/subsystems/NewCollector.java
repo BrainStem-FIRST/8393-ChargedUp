c897b19b8a0190a3826597452d4aba6c2e68d102
Ryan Scott <rydj007@gmail.com>
2023-03-30 10:15:39 -0400

    added distance sensor to NewCollector

--- src/main/java/frc/robot/subsystems/NewCollector.java
+++ src/main/java/frc/robot/subsystems/NewCollector.java
@@ -2,11 +2,16 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.Rev2mDistanceSensor.Port;
+import com.revrobotics.Rev2mDistanceSensor.RangeProfile;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import edu.wpi.first.wpilibj.CAN;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utilities.BrainSTEMSubsystem;
+
 
 public class NewCollector extends SubsystemBase implements BrainSTEMSubsystem{
 
@@ -20,6 +25,54 @@ public class NewCollector extends SubsystemBase implements BrainSTEMSubsystem{
         public static boolean k_rightMotorInverted = false;
         public static boolean k_leftMotorInverted = false;
 
+    }
+
+    private static final String profileDefault = "Default";
+    private static final String highSpeed = "High Speed";
+    private static final String highAccuracy = "High Accuracy";
+    private static final String longRange = "Long Range";
+    private String m_profileSelected;
+    private final SendableChooser<String> m_chooser = new SendableChooser<>();
+    
+    private Rev2mDistanceSensor distSens; //look for why is this not importing correctly
+
+@Override
+public void initialize() {
+    m_collectorState = CollectorState.OFF;
+    m_chooser.setDefaultOption("Default", profileDefault);
+    m_chooser.addOption("High Speed", highSpeed);
+    m_chooser.addOption("High Accuracy", highAccuracy);
+    m_chooser.addOption("Long Range", longRange);
+    SmartDashboard.putData("Profile", m_chooser);
+    distSens = new Rev2mDistanceSensor(Port.kOnboard);
+    distSens.setAutomaticMode(true);
+    distSens.setEnabled(true);
+}
+
+@Override
+public void periodic() {
+    setCollectorState();
+
+    switch (m_profileSelected) {
+        case highSpeed:
+          distSens.setRangeProfile(RangeProfile.kHighSpeed);
+          break;
+        case highAccuracy:
+          distSens.setRangeProfile(RangeProfile.kHighAccuracy);
+          break;
+        case longRange:
+          distSens.setRangeProfile(RangeProfile.kLongRange);
+          break;
+        default:
+          distSens.setRangeProfile(RangeProfile.kDefault);
+          break;
+      }
+
+    boolean isValid = distSens.isRangeValid();
+    SmartDashboard.putBoolean("Valid", isValid);
+    if(isValid) {
+        SmartDashboard.putNumber("Range", distSens.getRange());
+        SmartDashboard.putNumber("Timestamp", distSens.getTimestamp());
     }
 
     private CANSparkMax m_rightMotor;
@@ -107,22 +160,11 @@ public class NewCollector extends SubsystemBase implements BrainSTEMSubsystem{
         }
     }
 
-    
-
-    @Override
-    public void initialize() {
-        m_collectorState = CollectorState.OFF;
-    }
-
-    @Override
-    public void periodic() {
-        setCollectorState();
-        
-    }
 
     @Override
     public void disablePeriodic() {
         m_collectorState = CollectorState.OFF;
+        distSens.setEnabled(false); //this might cause a problem, not sure until tested 
     }
 
 
